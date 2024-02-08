aa4464eec2cd3df7918d95724556115797c11189
OksanaLukashyk <130361592+OksanaLukashyk@users.noreply.github.com>
2024-02-09 00:02:14 +0200

    06

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,10 +2,18 @@ require("dotenv").config();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
-const HttpError = require("../helpers/HttpError");
-const TryCatch = require("../helpers/TryCatch");
+const { nanoid } = require("nanoid");
 const User = require("../models/User");
-const { authUserSchema, subscrSchema } = require("../schema/authSchema");
+const {
+  authUserSchema,
+  subscrSchema,
+  verifyEmailSchema,
+} = require("../schema/authSchema");
+const HttpError = require("../helpers/HttpError");
+const sendEmail = require("../helpers/SendEmail");
+const TryCatch = require("../helpers/TryCatch");
+
+const { BASE_URL } = process.env;
 
 const register = async (req, res, next) => {
   const { error } = authUserSchema.validate(req.body);
@@ -23,11 +31,22 @@ const register = async (req, res, next) => {
 
   const hashedPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
+  const verificationToken = nanoid();
+
+  const verifyMsg = {
+    to: email,
+    subject: "Сonfirm your registration",
+    html: `<a target="_blank" href="${BASE_URL}/users/verify/${verificationToken}">Click to confirm your registration</a>`,
+    text: `Open the link to confirm your registration: ${BASE_URL}/users/verify/${verificationToken}`,
+  };
+
+  await sendEmail(verifyMsg);
 
   const newUser = await User.create({
     ...req.body,
     password: hashedPassword,
     avatarURL,
+    verificationToken,
   });
 
   res.status(201).json({
@@ -35,7 +54,60 @@ const register = async (req, res, next) => {
       email: newUser.email,
       subscription: newUser.subscription,
       avatarURL: newUser.avatarURL,
+      verificationToken: newUser.verificationToken,
     },
+  });
+};
+
+const verify = async (req, res) => {
+  const { verificationToken } = req.params;
+
+  const user = await User.findOne({ verificationToken });
+
+  if (!user) {
+    throw HttpError(404, "User not found");
+  }
+
+  await User.findByIdAndUpdate(user._id, {
+    verify: true,
+    verificationToken: null,
+  });
+
+  res.status(200).json({
+    message: "Verification successful",
+  });
+};
+
+const resendVerify = async (req, res) => {
+  const { error } = verifyEmailSchema.validate(req.body);
+
+  if (error) {
+    throw HttpError(400, error.message);
+  }
+
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    throw HttpError(404, "User not found");
+  }
+
+  if (user.verify) {
+    throw HttpError(400, "Verification has already been passed");
+  }
+
+  const verifyMsg = {
+    to: email,
+    subject: "Сonfirm your registration",
+    html: `<a target="_blank" href="${BASE_URL}/users/verify/${user.verificationToken}">Click to confirm your registration</a>`,
+    text: `Open the link to confirm your registration: ${BASE_URL}/users/verify/${user.verificationToken}`,
+  };
+
+  await sendEmail(verifyMsg);
+
+  res.status(200).json({
+    message: "Verification email sent",
   });
 };
 
@@ -51,6 +123,10 @@ const login = async (req, res, next) => {
 
   if (user === null) {
     throw HttpError(401, "Email or password is wrong");
+  }
+
+  if (!user.verify) {
+    throw HttpError(401, "Your email isn't verified");
   }
 
   const isMatch = await bcrypt.compare(password, user.password);
@@ -104,6 +180,8 @@ const updSubscription = async (req, res) => {
 
 module.exports = {
   register: TryCatch(register),
+  verify: TryCatch(verify),
+  resendVerify: TryCatch(resendVerify),
   login: TryCatch(login),
   logout: TryCatch(logout),
   getCurrent: TryCatch(getCurrent),
--- schema/contactSchema.js
+++ schema/contactSchema.js
@@ -1,10 +1,13 @@
 const Joi = require("joi");
 
+const emailRegexp = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
 const addSchema = Joi.object({
   name: Joi.string().required().messages({
     "any.required": "missing required name field",
   }),
   email: Joi.string()
+    .pattern(emailRegexp)
     .required()
     .messages({ "any.required": "missing required email field" }),
   phone: Joi.string()
@@ -14,7 +17,7 @@ const addSchema = Joi.object({
 
 const updSchema = Joi.object({
   name: Joi.string(),
-  email: Joi.string(),
+  email: Joi.string().pattern(emailRegexp),
   phone: Joi.string(),
 });
 
